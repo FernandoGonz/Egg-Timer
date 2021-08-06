99e1245669689401427c436bd5c7f7af83454fbc
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-08-06 13:28:37 -0500

    Initial Commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Fernando Guerrero G on 06/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
